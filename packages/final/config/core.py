69d356d3520759be53347557e74f199add34a6d8
samrat <samrat.tidke@atos.net>
2022-09-07 18:21:20 +0530

    changes

--- packages/final/config/core.py
+++ packages/final/config/core.py
@@ -10,6 +10,7 @@ from strictyaml import YAML, load
 PACKAGE_ROOT = Path(__file__).resolve().parent.parent
 ROOT = PACKAGE_ROOT.parent
 CONFIG_FILE_PATH = PACKAGE_ROOT / "config.yml"
+print(CONFIG_FILE_PATH)
 DATASET_DIR = PACKAGE_ROOT / "datasets"
 TRAINED_MODEL_DIR = PACKAGE_ROOT / "trained_models"
 GCP_BUCKET_MODEL = PACKAGE_ROOT / "model"
